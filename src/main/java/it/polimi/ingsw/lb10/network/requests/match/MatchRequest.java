35a9a762d039eb12aa0a37f5cecadd6060fb5b3f
gui <guido.cestele@mail.polimi.it>
2024-04-02 19:14:28 +0200

    added implo of first login request/response handling2

--- src/main/java/it/polimi/ingsw/lb10/network/requests/match/MatchRequest.java
+++ src/main/java/it/polimi/ingsw/lb10/network/requests/match/MatchRequest.java
@@ -1,7 +1,6 @@
 package it.polimi.ingsw.lb10.network.requests.match;
 
 import it.polimi.ingsw.lb10.network.requests.Request;
-import it.polimi.ingsw.lb10.server.visitors.requestDispatch.RequestHandler;
 import it.polimi.ingsw.lb10.server.visitors.requestDispatch.RequestVisitor;
 
 public class MatchRequest extends Request {
--- src/main/java/it/polimi/ingsw/lb10/network/requests/preMatch/LoginRequest.java
+++ src/main/java/it/polimi/ingsw/lb10/network/requests/preMatch/LoginRequest.java
@@ -1,6 +1,5 @@
 package it.polimi.ingsw.lb10.network.requests.preMatch;
 import it.polimi.ingsw.lb10.network.requests.Request;
-import it.polimi.ingsw.lb10.server.visitors.requestDispatch.RequestHandler;
 import it.polimi.ingsw.lb10.server.visitors.requestDispatch.RequestVisitor;
 
 public class LoginRequest extends Request {
