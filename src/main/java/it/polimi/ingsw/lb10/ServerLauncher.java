7bdb83a9433cfcb1aeda54e1f9861fc2fae3a846
Lorenzo-Pigato <lorenzo.pigato@mail.polimi.it>
2024-04-03 23:56:03 +0200

    Added server CLI - to be finished

--- src/main/java/it/polimi/ingsw/lb10/ServerLauncher.java
+++ src/main/java/it/polimi/ingsw/lb10/ServerLauncher.java
@@ -1,5 +1,11 @@
 package it.polimi.ingsw.lb10;
 
+import it.polimi.ingsw.lb10.client.cli.CLIBanner;
+import it.polimi.ingsw.lb10.client.cli.CLIBox;
+import it.polimi.ingsw.lb10.client.cli.CLICommand;
+import it.polimi.ingsw.lb10.client.cli.ansi.AnsiColor;
+import it.polimi.ingsw.lb10.client.cli.ansi.AnsiFormat;
+import it.polimi.ingsw.lb10.client.cli.ansi.AnsiString;
 import it.polimi.ingsw.lb10.server.Server;
 
 import java.io.IOException;
@@ -7,14 +13,25 @@ import java.net.ServerSocket;
 
 public class ServerLauncher {
     public static void main(String[] args) {
-
         try {
             ServerSocket serverSocket = new ServerSocket(Integer.parseInt(args[1]));
             Thread serverThread = new Thread(new Server(serverSocket, Integer.parseInt(args[1])));
             serverThread.start();
             serverThread.join();
         } catch (Exception e) {
-            System.out.println(e.getMessage());
+            CLICommand.initialize(80, 50);
+            CLIBanner.displayError(17,2);
+
+            CLICommand.setPosition(2, 14);
+
+            AnsiString.print(">> Server running on port: ", AnsiColor.CYAN);
+            AnsiString.print(args[1] + "\t", AnsiColor.YELLOW, AnsiFormat.BOLD);
+
+            AnsiString.print("Status: ", AnsiColor.CYAN);
+            AnsiString.print("OFFLINE", AnsiColor.RED, AnsiFormat.BOLD);
+
+            CLICommand.setPosition(2, 17);
+            AnsiString.print(">> Error: " + e.getMessage() + "\n\n", AnsiColor.RED);
         }
     }
 }
--- src/main/java/it/polimi/ingsw/lb10/server/Server.java
+++ src/main/java/it/polimi/ingsw/lb10/server/Server.java
@@ -1,4 +1,11 @@
 package it.polimi.ingsw.lb10.server;
+import it.polimi.ingsw.lb10.client.cli.CLIBanner;
+import it.polimi.ingsw.lb10.client.cli.CLIBox;
+import it.polimi.ingsw.lb10.client.cli.CLICommand;
+import it.polimi.ingsw.lb10.client.cli.CLIString;
+import it.polimi.ingsw.lb10.client.cli.ansi.AnsiColor;
+import it.polimi.ingsw.lb10.client.cli.ansi.AnsiFormat;
+import it.polimi.ingsw.lb10.client.cli.ansi.AnsiString;
 import it.polimi.ingsw.lb10.network.ClientConnection;
 import it.polimi.ingsw.lb10.server.controller.LobbyController;
 import it.polimi.ingsw.lb10.server.model.MatchModel;
@@ -29,16 +36,48 @@ public class Server implements Runnable{
 
     //starts the Server thread listening to all client connections, then sends socket client to connection handlers
     public void run(){
-
+        setPage();
         while(true){
             try {
-                System.out.println("Server listening on port: " + port);
                 Socket clientSocket = serverSocket.accept();
                 welcomeExecutor.submit(new ClientConnection(clientSocket, this));
             }catch(IOException e){
-                System.out.println(">>>Server: Couldn't connect to Client...");
+                displayError();
+                CLICommand.setPosition(2, 49);
+                AnsiString.print(">> Error: " + e.getMessage(), AnsiColor.RED);
             }
         }
+    }
+
+    private void setPage() {
+        CLICommand.initialize(80, 50);
+        CLIBanner.displayServer();
+        CLIBox.draw(1,15,80,30, AnsiColor.CYAN);
+
+        CLICommand.setPosition(2,14);
+        AnsiString.print(">> Server running on port: ", AnsiColor.CYAN);
+        AnsiString.print(this.getPort() + "\t", AnsiColor.YELLOW, AnsiFormat.BOLD);
+
+        AnsiString.print("Status: ", AnsiColor.CYAN);
+        AnsiString.print("ONLINE", AnsiColor.GREEN, AnsiFormat.BOLD);
+
+        CLICommand.setInvisibleInput();
+    }
+
+    private void displayError(){
+        CLICommand.home();
+        CLICommand.clearNextLines(14);
+        CLIBanner.displayError(17,2);
+
+        CLICommand.setPosition(2, 14);
+
+        AnsiString.print(">> Server running on port: ", AnsiColor.CYAN);
+        AnsiString.print(this.getPort() + "\t", AnsiColor.YELLOW, AnsiFormat.BOLD);
+
+        AnsiString.print("Status: ", AnsiColor.CYAN);
+        AnsiString.print("OFFLINE", AnsiColor.RED, AnsiFormat.BOLD);
+
+        CLICommand.resetInvisibleInput();
     }
 
 }
