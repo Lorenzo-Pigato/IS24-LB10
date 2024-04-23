145f4b50a6a59809acb7543e6adf4c28177da21a
SimoneBottaro <simonebottaro29@gmail.com>
2024-04-23 14:41:31 +0200

    Quest

---
+++ src/test/java/it/polimi/ingsw/lb10/server/model/MatchModelTest.java
@@ -0,0 +1,89 @@
+package it.polimi.ingsw.lb10.server.model;
+
+import it.polimi.ingsw.lb10.server.controller.MatchController;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MatchModelTest {
+    static MatchModel matchModel;
+    static MatchController matchController;
+    static Player player1;
+    static Player player2 ;
+    static Player player3 ;
+
+    static ArrayList<Player> players= new ArrayList<>();
+    @BeforeAll
+    static void setUp(){
+        matchController=new MatchController(3);
+        matchModel=new MatchModel(3,matchController);
+        player1= new Player(123, "TestPlayer1");
+        player2= new Player(456, "TestPlayer2");
+        player3= new Player(789, "TestPlayer3");
+        players.add(player1);
+        players.add(player2);
+        players.add(player3);
+        matchModel.setPlayers(players);
+    }
+
+    /**
+     * 321ms
+     */
+    @Test
+    void initializeDecks() throws IOException {
+        matchModel.initializeDecks();
+        assertFalse(matchModel.getResourceDeck().getCards().isEmpty());
+        assertEquals(40, matchModel.getResourceDeck().getCards().size());
+
+        assertFalse(matchModel.getGoldenDeck().getCards().isEmpty());
+        assertEquals(40, matchModel.getGoldenDeck().getCards().size());
+
+        assertFalse(matchModel.getQuestDeck().getCards().isEmpty());
+        assertEquals(16, matchModel.getQuestDeck().getCards().size());
+
+        assertFalse(matchModel.getStartingDeck().getCards().isEmpty());
+        assertEquals(6, matchModel.getStartingDeck().getCards().size());
+    }
+
+    /**
+     * There isn't a method to set up the players inside the match model by constructor!
+     */
+    @Test
+    void initializeMatrix(){
+        matchModel.initializeMatrix();
+        assertNotNull(player1.getMatrix());
+        assertNotNull(player2.getMatrix());
+        assertNotNull(player3.getMatrix());
+    }
+
+
+    @Test
+    void initializeCardsOnHand() {
+        matchModel.initializeCardsOnHand();
+        assertNotNull(player1.getHand());
+        assertNotNull(player2.getHand());
+        assertNotNull(player3.getHand());
+
+        assertEquals(3,player1.getHand().size());
+        assertEquals(3,player2.getHand().size());
+        assertEquals(3,player3.getHand().size());
+
+    }
+
+    @Test
+    void startingUncoveredCards(){
+        matchModel.startingUncoveredCards();
+        assertNotNull(matchModel.getGoldenUncovered());
+        assertNotNull(matchModel.getResourceUncovered());
+        assertNotNull(matchModel.getCommonQuests());
+
+        assertEquals(2,matchModel.getGoldenUncovered().size());
+        assertEquals(2,matchModel.getResourceUncovered().size());
+        assertEquals(2,matchModel.getCommonQuests().size());
+    }
+
+}
---
+++ src/test/java/it/polimi/ingsw/lb10/server/model/quest/QuestTest.java
@@ -0,0 +1,59 @@
+package it.polimi.ingsw.lb10.server.model.quest;
+
+import it.polimi.ingsw.lb10.server.model.Player;
+import it.polimi.ingsw.lb10.server.model.Resource;
+import it.polimi.ingsw.lb10.server.model.cards.decks.QuestDeck;
+import org.junit.jupiter.api.*;
+
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.Map;
+
+import static org.junit.Assert.*;
+
+class QuestCounterTest {
+    private static QuestDeck questDeck = new QuestDeck();
+    private static Quest counterQuest;
+    private static Player player;
+    private static Map<Resource,Integer> activationCost = new HashMap<>();
+    private static Map<Resource,Integer> onMapResources= new HashMap<>();
+
+    @BeforeAll
+    static void setUp(){
+        questDeck.fillDeck();
+        counterQuest=questDeck.getCards().getFirst();
+        player=new Player(123,"Tony");
+    }
+
+    @AfterEach
+    void cleanUp(){
+        player.getOnMapResources().clear();
+    }
+
+    @Test
+    void algorithmResourcesNotInTheQuest() {
+        onMapResources.put(Resource.MUSHROOM,10);
+        for(int i=0;i<onMapResources.get(Resource.MUSHROOM);i++)
+            player.addOnMapResources(Resource.MUSHROOM);
+        Assertions.assertEquals(0, counterQuest.questAlgorithm(player.getOnMapResources()));
+    }
+
+    @Test
+    void algorithmResourcesInQuest() {
+        onMapResources.put(Resource.INSECT,10);
+        for(int i=0;i<onMapResources.get(Resource.INSECT);i++)
+            player.addOnMapResources(Resource.INSECT);
+        Assertions.assertEquals(6, counterQuest.questAlgorithm(player.getOnMapResources()));
+    }
+
+    /**
+     * test on card 94
+     */
+    @Test
+    void algorithmsDifferentResources(){
+        counterQuest=questDeck.getCards().get(7);
+
+    }
+
+
+}
