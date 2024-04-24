395921f08b64e69caaa85a95c5092c421fe52cce
lorenzo.pigato <lorenzo.pigato@mail.polimi.it>
2024-04-24 15:28:56 +0200

    Edited CLIBanner class with text blocks, working on CLIStartMatchPage

--- src/main/java/it/polimi/ingsw/lb10/client/cli/clipages/CLIStartMatchPage.java
+++ src/main/java/it/polimi/ingsw/lb10/client/cli/clipages/CLIStartMatchPage.java
@@ -1,6 +1,7 @@
 package it.polimi.ingsw.lb10.client.cli.clipages;
 
 import it.polimi.ingsw.lb10.client.cli.CLIBanner;
+import it.polimi.ingsw.lb10.client.cli.CLIBox;
 import it.polimi.ingsw.lb10.client.cli.CLICommand;
 import it.polimi.ingsw.lb10.client.cli.CLIString;
 import it.polimi.ingsw.lb10.client.cli.ansi.AnsiColor;
@@ -8,8 +9,9 @@ import it.polimi.ingsw.lb10.client.cli.ansi.AnsiFormat;
 import org.jetbrains.annotations.NotNull;
 
 public class CLIStartMatchPage implements CLIPage {
-    private static final CLIString notFound = new CLIString(">> Server couldn't be reached <<", AnsiColor.CYAN, AnsiFormat.BOLD, 1, 33);
-    private static final CLIString reconnect = new CLIString(">> Quitting <<", AnsiColor.CYAN, 1, 35);
+    private static final CLIString notFound = new CLIString(">> Choose [1] or [2] <<", AnsiColor.RED, AnsiFormat.BOLD, 1, 38);
+    private static final CLIString chooseQuest = new CLIString(">> Choose a quest to start the match <<", AnsiColor.CYAN, AnsiFormat.BOLD, 1, 38);
+
     private CLIState state = new CLIStartMatchPage.Default();
 
     @Override
@@ -26,6 +28,11 @@ public class CLIStartMatchPage implements CLIPage {
         @Override
         public void apply(Object[] args) {
             CLICommand.initialize();
+            CLIBanner.displayChooseQuest();
+
+            CLIBox.draw(30,21,40,15,AnsiColor.WHITE);
+            CLIBox.draw(90,21,40,15,AnsiColor.WHITE);
+
 
         }
 
