92ef97607e1247b49e7b94fd36678dbafc4a9349
lorenzo.pigato <lorenzo.pigato@mail.polimi.it>
2024-05-03 14:42:52 +0200

    Starting gui

---
+++ src/main/java/it/polimi/ingsw/lb10/client/gui/ClientGUI.java
@@ -0,0 +1,16 @@
+package it.polimi.ingsw.lb10.client.gui;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class ClientGUI extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
