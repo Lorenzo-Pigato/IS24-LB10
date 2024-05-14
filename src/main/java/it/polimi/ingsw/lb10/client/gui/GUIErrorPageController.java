d7e6751f94abb1b87e184f1e8633537e1092b00f
lorenzo.pigato <lorenzo.pigato@mail.polimi.it>
2024-05-14 09:05:32 +0200

    added page controllers

---
+++ src/main/java/it/polimi/ingsw/lb10/client/gui/GUIErrorPageController.java
@@ -0,0 +1,33 @@
+package it.polimi.ingsw.lb10.client.gui;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.text.Text;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class GUIErrorPageController implements GUIPageController, Initializable {
+
+    @FXML
+    private Text errorText;
+
+    @Override
+    public String getFXML() {
+        return "/fxml/ErrorPage.fxml";
+    }
+
+    @Override
+    public String getCSS() {
+        return "/css/ErrorPage.css";
+    }
+
+    public void setErrorText(String error){
+        errorText.setText(error);
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
