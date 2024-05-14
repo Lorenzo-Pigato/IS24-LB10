d5c273a9fe3ef9b366cd6795bfdea61ae8ec1468
lorenzo.pigato <lorenzo.pigato@mail.polimi.it>
2024-05-14 09:16:07 +0200

    added exception handler

--- src/main/java/it/polimi/ingsw/lb10/client/exception/GUIExceptionHandler.java
+++ src/main/java/it/polimi/ingsw/lb10/client/exception/GUIExceptionHandler.java
@@ -1,35 +1,53 @@
 package it.polimi.ingsw.lb10.client.exception;
 
 import it.polimi.ingsw.lb10.client.controller.GUIClientViewController;
+import it.polimi.ingsw.lb10.client.gui.GUIErrorPageController;
+import javafx.application.Platform;
 
 import java.io.IOException;
 import java.net.UnknownHostException;
 
 public class GUIExceptionHandler implements ExceptionHandler{
 
-    private final GUIClientViewController guiClientViewController;
+    private final GUIClientViewController controller;
 
     public GUIExceptionHandler(GUIClientViewController guiClientViewController) {
-        this.guiClientViewController = guiClientViewController;
+        this.controller = guiClientViewController;
     }
 
     @Override
     public void handle(Exception e) {
-
+        Platform.runLater(() -> {
+            GUIErrorPageController errorPage = new GUIErrorPageController();
+            controller.changeScene(errorPage);
+            errorPage.setErrorText("An Exception occurred: " + e.getMessage());
+        });
     }
 
     @Override
     public void handle(UnknownHostException e) {
-
+        Platform.runLater(() -> {
+            GUIErrorPageController errorPage = new GUIErrorPageController();
+            controller.changeScene(errorPage);
+            errorPage.setErrorText("An Exception occurred: " + e.getMessage());
+        });
     }
 
     @Override
     public void handle(IOException e) {
-
+        Platform.runLater(() -> {
+            GUIErrorPageController errorPage = new GUIErrorPageController();
+            controller.changeScene(errorPage);
+            errorPage.setErrorText("There was an issue with your sockets: " + e.getMessage());
+        });
     }
 
     @Override
     public void handle(ConnectionErrorException e) {
-
+        Platform.runLater(() -> {
+            GUIErrorPageController errorPage = new GUIErrorPageController();
+            controller.changeScene(errorPage);
+            errorPage.setErrorText("There were some issues connecting to the server: " + e.getMessage());
+        });
     }
 }
