2346a9016f20f673657eb8be578aa4d8c3f1bca9
lorenzo.pigato <lorenzo.pigato@mail.polimi.it>
2024-05-21 17:18:25 +0200

    fixing shadows in guiMatchPage

--- src/main/java/it/polimi/ingsw/lb10/client/gui/GUIMatchPageController.java
+++ src/main/java/it/polimi/ingsw/lb10/client/gui/GUIMatchPageController.java
@@ -449,6 +449,7 @@ public class GUIMatchPageController implements GUIPageController , Initializable
     private void validatePlacing(@NotNull Rectangle hooverRectangle) {
         controller.send(new PlaceCardRequest(controller.getMatchId(), clickedCard, (Position)(hooverRectangle.getUserData()), matrixCardId)); //send place request
         placeRequestValidating = true;
+        resetAllBoardShadows();
     }
 
     private static void hooverRectangleStyleSetup(Rectangle hooverRectangle) {
